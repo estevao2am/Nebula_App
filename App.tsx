6ae49d3db6765de626147467dd9029d1fc74e890
user <user@Estevao-Esteves.local>
2025-08-02 09:51:50 +0100

    first comit with fonts fixed

--- App.tsx
+++ App.tsx
@@ -1,28 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { Text } from './src/components/Text';
+import { theme } from './src/theme/theme';
+import { ThemeProvider } from '@shopify/restyle';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingLarge">Nebula App</Text>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
